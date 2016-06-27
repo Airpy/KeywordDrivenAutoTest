24c9547ba76dd39efb686395702e751945a2a7e4
Airpy <getimer@gmail.com>
2016-06-27 15:28:43 +0800

    edit Constants.java

--- src/main/java/com/keyword/automation/base/common/Constants.java
+++ src/main/java/com/keyword/automation/base/common/Constants.java
@@ -2,6 +2,7 @@ package com.keyword.automation.base.common;
 
 /**
  * 系统常量类
+ * 
  * @author airpy
  *
  */
@@ -10,4 +11,9 @@ public class Constants {
 	 * Chrome浏览器driver的存放路径
 	 */
 	public static final String CHROME_PATH = "/src/static/driver/chromedriver.exe";
+
+	/**
+	 * IE浏览器driver的存放路径
+	 */
+	public static final String IE_PATH = "/src/static/driver/IEDriverServer.exe";
 }
