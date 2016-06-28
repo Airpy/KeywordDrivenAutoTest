a8ce2f2dbc8dc279d1d96a73610e5815a45a46c7
Airpy <getimer@gmail.com>
2016-06-29 00:20:54 +0800

    add browser keyword file

---
+++ src/main/java/com/keyword/automation/action/BrowserKeyword.java
@@ -0,0 +1,13 @@
+package com.keyword.automation.action;
+
+/**
+ * 封装浏览器操作关键字
+ * 
+ * @author liuzhenfeng
+ *
+ */
+public class BrowserKeyword {
+	public static void browserOpen(String browserType, String requestUrl) {
+
+	}
+}
