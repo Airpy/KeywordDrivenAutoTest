fb703e0fe35c7707ff710e3c0368c4599b94ebf8
Airpy <getimer@gmail.com>
2016-06-29 22:11:20 +0800

    edit browser action files
    add some browser action keyword

--- src/main/java/com/keyword/automation/action/BrowserKeyword.java
+++ src/main/java/com/keyword/automation/action/BrowserKeyword.java
@@ -37,6 +37,194 @@ public class BrowserKeyword {
 		String reqUrl = verifyRequestUrl(requestUrl);
 		Browsers.createDriver(bType).open(reqUrl);
 	}
+	
+	/**
+	 * 获取当前打开标签页Url
+	 * 
+	 * @return 当前打开标签页Url
+	 */
+	public static String getCurrentUrl() {
+		return Browsers.getActiveBrowser().getCurrentUrl();
+	}
+
+	/**
+	 * 获取当前打开标签页的title
+	 * 
+	 * @return 当前打开标签页的title
+	 */
+	public static String getCurrentTitle() {
+		return Browsers.getActiveBrowser().getCurrentTitle();
+	}
+
+	/**
+	 * 获取当前打开标签页的源码
+	 * 
+	 * @return 当前打开标签页的源码
+	 */
+	public static String getPageSource() {
+		return Browsers.getActiveBrowser().getPageSource();
+	}
+
+	/**
+	 * 关闭当前标签页
+	 */
+	public static void browserClose() {
+		Browsers.getActiveBrowser().browserClose();
+	}
+
+	/**
+	 * 退出浏览器，关闭所有标签页
+	 */
+	public static void browserQuit() {
+		Browsers.getActiveBrowser().browserQuit();
+	}
+
+	/**
+	 * 当打开新标签页时，切换到新窗口
+	 */
+	public static void switchToWindow() {
+		Browsers.getActiveBrowser().switchToWindow();
+	}
+
+	/**
+	 * 当存在多标签页时，切换到指定窗口
+	 * 
+	 * @param windowTitle
+	 *            指定窗口的Title
+	 */
+	public static void switchToWindow(String windowTitle) {
+		Browsers.getActiveBrowser().switchToWindow(windowTitle);
+	}
+
+	/**
+	 * 根据frame的id或name切换
+	 * 
+	 * @param obj
+	 *            frame的id或name
+	 */
+	public static void switchToFrame(Object obj) {
+		Browsers.getActiveBrowser().switchToFrame(obj);
+	}
+
+	/**
+	 * 通过页面元素切换frame
+	 * 
+	 * @param locator
+	 *            传id/name/linkText等
+	 * @param locatorValue
+	 *            元素的值
+	 */
+	public static void switchToFrame(String locator, String locatorValue) {
+		Browsers.getActiveBrowser().switchToFrame(locator, locatorValue);
+	}
+
+	/**
+	 * 浏览器后退导航
+	 */
+	public static void browserBack() {
+		Browsers.getActiveBrowser().browserBack();
+	}
+
+	/**
+	 * 浏览器前进导航
+	 */
+	public static void browserForward() {
+		Browsers.getActiveBrowser().browserForward();
+	}
+
+	/**
+	 * 浏览器刷新
+	 */
+	public static void browserRefresh() {
+		Browsers.getActiveBrowser().browserRefresh();
+	}
+
+	/**
+	 * 最大化浏览器
+	 */
+	public static void browserMax() {
+		Browsers.getActiveBrowser().browserMax();
+	}
+
+	/**
+	 * 添加Cookie
+	 * @param cookieName  Cookie名称
+	 * @param CookieValue  Cookie值
+	 */
+	public static void addCookie(String cookieName, String CookieValue) {
+		Browsers.getActiveBrowser().addCookie(cookieName, CookieValue);
+	}
+
+	/**
+	 * 通过Cookie名称删除对应Cookie
+	 * 
+	 * @param cookieName
+	 *            Cookie名称
+	 */
+	public static void deleteCookieByName(String cookieName) {
+		Browsers.getActiveBrowser().deleteCookieByName(cookieName);
+	}
+
+	/**
+	 * 删除所有Cookie
+	 */
+	public static void deleteAllCookies() {
+		Browsers.getActiveBrowser().deleteAllCookies();
+	}
+
+	/**
+	 * 获取所有Cookie
+	 * 
+	 * @return
+	 */
+	public static String getAllCookies() {
+		return null;
+	}
+
+	/**
+	 * 从Cookie文件中获取所有Cookie
+	 * 
+	 * @param cookieFile
+	 *            Cookie文件名称
+	 * @return
+	 */
+	public static String getAllCookies(String cookieFile) {
+		return null;
+	}
+
+	/**
+	 * 根据Cookie名称获取Cookie值
+	 * 
+	 * @param cookieName
+	 *            Cookie名称
+	 * @return
+	 */
+	public static String getCookieByName(String cookieName) {
+		return null;
+	}
+
+	/**
+	 * 从Cookie文件中通过Cookie名称获取Cookie值
+	 * 
+	 * @param cookieFile
+	 *            Cookie文件名称
+	 * @param cookieName
+	 *            Cookie名称
+	 * @return
+	 */
+	public static String getCookieByName(String cookieFile, String cookieName) {
+		return null;
+	}
+
+	/**
+	 * 将所有Cookie写入Cookie文件
+	 * 
+	 * @param CookieFile
+	 *            Cookie文件名称
+	 */
+	public static void writeAllCookiesToFile(String CookieFile) {
+
+	}
 
 	/**
 	 * 验证传入浏览器类型
--- src/main/java/com/keyword/automation/base/browser/WebBrowser.java
+++ src/main/java/com/keyword/automation/base/browser/WebBrowser.java
@@ -1,9 +1,22 @@
 package com.keyword.automation.base.browser;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.keyword.automation.base.utils.LogUtils;
 
+/**
+ * 浏览器操作实现类
+ * 
+ * @author airpy
+ *
+ */
 public class WebBrowser {
 	protected WebDriver driver;
 
@@ -22,5 +35,110 @@ public class WebBrowser {
 		} catch (Exception e) {
 			LogUtils.error("browser does not support maximize");
 		}
+	}
+
+	public String getCurrentUrl() {
+		return this.driver.getCurrentUrl();
+	}
+
+	public String getCurrentTitle() {
+		return this.driver.getTitle();
+	}
+
+	public String getPageSource() {
+		return this.driver.getPageSource();
+	}
+
+	public void browserClose() {
+		this.driver.close();
+	}
+
+	public void browserQuit() {
+		this.driver.quit();
+	}
+
+	public void switchToWindow() {
+		String currentWindow = this.driver.getWindowHandle();
+		Set<String> windowHandles = this.driver.getWindowHandles();
+		Iterator<String> it = windowHandles.iterator();
+		while (it.hasNext()) {
+			if (currentWindow == it.next())
+				continue;
+			this.driver.switchTo().window(it.next());
+		}
+	}
+
+	public void switchToWindow(String windowTitle) {
+		long start = System.currentTimeMillis();
+		long maxWaitTime = 15000L;
+		do {
+			try {
+				Set<String> windowHandles = this.driver.getWindowHandles();
+				LogUtils.debug("all window handles: " + windowHandles);
+				for (String windowHandle : windowHandles) {
+					this.driver.switchTo().window(windowHandle);
+					String currentTitle = this.driver.getTitle();
+					LogUtils.info(
+							"Try to check window <title: " + currentTitle + ", expect window: " + windowHandle + ">.");
+					if (((windowTitle.equals("")) && (currentTitle.equals("")))
+							|| ((!windowTitle.equals("")) && (currentTitle.contains(windowTitle)))) {
+						LogUtils.debug("swith to window: " + currentTitle + "success.");
+						return;
+					}
+				}
+			} catch (Exception e) {
+				LogUtils.debug("Ignored Exception");
+			}
+		} while (System.currentTimeMillis() - start < maxWaitTime);
+		throw new NoSuchWindowException("Window <Title:" + windowTitle + "> is not exist in browser.");
+	}
+
+	public void switchToFrame(Object obj) {
+		if (obj instanceof String) {
+			this.driver.switchTo().frame(obj.toString());
+		} else if (obj instanceof Integer) {
+			this.driver.switchTo().frame(Integer.parseInt(obj.toString()));
+		}
+	}
+
+	public void switchToFrame(String locator, String locatorValue) {
+
+	}
+
+	public void browserBack() {
+		this.driver.navigate().back();
+	}
+
+	public void browserForward() {
+		this.driver.navigate().forward();
+	}
+
+	public void browserRefresh() {
+		this.driver.navigate().refresh();
+	}
+
+	public void browserMax() {
+		this.driver.manage().window().maximize();
+	}
+
+	public void addCookie(String cookieName, String CookieValue) {
+		Cookie cookie = new Cookie(cookieName, CookieValue);
+		this.driver.manage().addCookie(cookie);
+	}
+
+	public void deleteCookieByName(String cookieName) {
+		this.driver.manage().deleteCookieNamed(cookieName);
+	}
+
+	public void deleteAllCookies() {
+		this.driver.manage().deleteAllCookies();
+	}
+
+	public WebElement findElement(String locator, String locatorValue) {
+		return null;
+	}
+
+	public List<WebElement> findElements(String locator, String locatorValue) {
+		return null;
 	}
 }
