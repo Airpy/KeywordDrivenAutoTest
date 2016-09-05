5ab0848406dba1d043ec0d6afdbf81837f0de330
Amio_ <getimer@gmail.com>
2016-09-05 20:27:23 +0800

    add json method

--- src/main/java/com/keyword/automation/base/utils/JsonUtils.java
+++ src/main/java/com/keyword/automation/base/utils/JsonUtils.java
@@ -1,211 +1,152 @@
 package com.keyword.automation.base.utils;
 
-import com.keyword.automation.bean.BillCell;
-import com.keyword.automation.bean.BillFooter;
-import com.keyword.automation.bean.BillHeader;
-import com.keyword.automation.bean.BillWhole;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 
-import java.util.*;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * JSON操作工具类
  *
  * @author Amio_
  */
-public class JsonUtils {
+public class JSONUtils {
     /**
-     * 将泛型List对象序列成JSON字符串
-     * before: [aaa, bbb, ccc]
-     * after: ["aaa","bbb","ccc"]
-     *
-     * @param list List对象
-     * @param <T>  泛型形参
-     * @return JSON字符串
-     */
-    public static <T> String toJsonStringFromList(List<T> list) {
-        JSONArray jsonArray = JSONArray.fromObject(list);
-        return jsonArray.toString();
-    }
-
-    /**
-     * 将指定对象序列化成JSON字符串
-     * before: Student{username='小明',sex='男',tel=['13999999991','02588888888'],Score{chinese=99,math=100}}
-     * after: [{"username":"小明","sex":"男","tel":["13999999991","02588888888"],"score":{"chinese":99,"math":100}}]
-     *
-     * @param object 指定对象
-     * @return JSON字符串
-     */
-    public static String toJsonStringFromObject(Object object) {
-        JSONObject jsonObject = JSONObject.fromObject(object);
-        return jsonObject.toString();
-//        JSONArray jsonArray = JSONArray.fromObject(object);
-//        return jsonArray.toString();
-    }
-
-    /**
-     * 将对象序列化成List
-     * before: Student{username='小明',sex='男',tel=['13999999991','02588888888'],Score{chinese=99,math=100}}
-     * after: [小明, 男, ["13999999991","02588888888"], {"chinese":99,"math":100}]
-     *
-     * @param object 指定对象
-     * @return List
-     */
-    public static List toListFromObject(Object object) {
-        List<Object> list = new ArrayList<>();
-        JSONArray jsonArray = JSONArray.fromObject(object);
-        for (Object aJsonArray : jsonArray) {
-            JSONObject jsonObject = (JSONObject) aJsonArray;
-            Iterator keys = jsonObject.keys();
-            while (keys.hasNext()) {
-                Object key = keys.next();
-                Object value = jsonObject.get(key);
-                list.add(value);
-            }
-        }
-        return list;
-    }
-
-    /**
-     * 将JSON对象数组序列化List
-     * before: [{"username":"小明","sex":"男","tel":["13999999991","02588888888"],"score":{"chinese":99,"math":100}}]
-     * after: [{score={math=100, chinese=99}, sex=男, username=小明, tel=[13999999991, 02588888888]}]
-     *
-     * @param jsonArray JSON对象数组
-     * @return List
-     */
-    public static List toListFromJson(JSONArray jsonArray) {
-        List<Object> list = new ArrayList<>();
-        for (Object obj : jsonArray) {
-            if (obj instanceof JSONArray) {
-                list.add(toListFromJson((JSONArray) obj));
-            } else if (obj instanceof JSONObject) {
-                list.add(toMapFromJson((JSONObject) obj));
-            } else {
-                list.add(obj);
-            }
-        }
-        return list;
-    }
-
-
-    /**
-     * 将JSON字符串序列化Map
-     * before: {"username":"小明","sex":"男","tel":["13999999991","02588888888"],"score":{"chinese":99,"math":100}}
-     * after: {score={math=100, chinese=99}, sex=男, username=小明, tel=[13999999991, 02588888888]}
-     *
-     * @param json JSON字符串
-     * @return Map
-     */
-    public static Map<String, Object> toMapFromJson(String json) {
-        JSONObject obj = JSONObject.fromObject(json);
-        return toMapFromJson(obj);
-    }
-
-
-    /**
-     * 将JSON对象序列化Map
-     * before: {"username":"小明","sex":"男","tel":["13999999991","02588888888"],"score":{"chinese":99,"math":100}}
-     * after: {score={math=100, chinese=99}, sex=男, username=小明, tel=[13999999991, 02588888888]}
-     *
-     * @param obj JSON对象
-     * @return Map
-     */
-    public static Map<String, Object> toMapFromJson(JSONObject obj) {
-        Set<?> set = obj.keySet();
-        Map<String, Object> map = new HashMap<>(set.size());
-        for (Object key : obj.keySet()) {
-            Object value = obj.get(key);
-            if (value instanceof JSONArray) {
-                map.put(key.toString(), toListFromJson((JSONArray) value));
-            } else if (value instanceof JSONObject) {
-                map.put(key.toString(), toMapFromJson((JSONObject) value));
-            } else {
-                map.put(key.toString(), obj.get(key));
-            }
-        }
-        return map;
-    }
-
-    /**
-     * 将JSON字符串转化为java对象
-     * before: {"username":"小明","sex":"男","tel":["13999999991","02588888888"],"score":{"chinese":99,"math":100}}
-     * after: Student{username='小明',sex='男',tel=['13999999991','02588888888'],Score{chinese=99,math=100}}
+     * 将JSON字符串序列化成JSONObject对象或JSONArray对象
      *
      * @param jsonString JSON字符串
-     * @param clazz      java类
-     * @return java对象
+     * @return JSONObject对象或JSONArray对象
      */
-    public static Object toBeanFromJsonString(String jsonString, Class clazz) {
-        JSONObject jsonObject = null;
+    public static Object toObjectFromJSONString(String jsonString) {
+        Object object = null;
         try {
-            jsonObject = JSONObject.fromObject(jsonString);
+            object = JSON.parse(jsonString);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return JSONObject.toBean(jsonObject, clazz);
+        return object;
     }
 
     /**
-     * 将JSON对象转化为java对象
-     * before: {"username":"小明","sex":"男","tel":["13999999991","02588888888"],"score":{"chinese":99,"math":100}}
-     * after: Student{username='小明',sex='男',tel=['13999999991','02588888888'],Score{chinese=99,math=100}}
+     * 将byte数组序列化成JSONObject对象或JSONArray对象
      *
-     * @param jsonObject JSON对象
-     * @param clazz      java类
-     * @return java对象
+     * @param bytes byte数组
+     * @return JSONObject对象或JSONArray对象
      */
-    public static Object toBeanFromJsonString(JSONObject jsonObject, Class clazz) {
-        return JSONObject.toBean(jsonObject, clazz);
+    public static Object toObjectFromByteArray(byte[] bytes) {
+        Object object = null;
+        try {
+            object = JSON.parse(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return object;
     }
 
-    public static void main(String[] args) {
-//        JSONObject jsonObject = new JSONObject();
-//        JSONObject subJSON = new JSONObject();
-//        JSONArray jsonArray = new JSONArray();
-//        jsonObject.put("username", "小明");
-//        jsonObject.put("sex", "男");
-//        jsonArray.add("13999999991");
-//        jsonArray.add("02588888888");
-//        jsonObject.put("tel", jsonArray);
-//        subJSON.put("chinese", 99);
-//        subJSON.put("math", 100);
-//        jsonObject.put("score", subJSON);
-//
-//        JSONArray jsonArray1 = new JSONArray();
-//        jsonArray1.add(jsonObject);
-//        System.out.println(JsonUtils.toListFromJson(jsonArray1));
-//
-//        String jsonStr = "{\"username\":\"小明\",\"sex\":\"男\",\"tel\":[\"13999999991\",\"02588888888\"]," +
-//                "\"score\":{\"chinese\":99,\"math\":100}}";
-//        System.out.println(JsonUtils.toMapFromJson(jsonStr));
-//
-//
-//        List<BillCell> billCellList = new ArrayList<>();
-//        BillHeader billHeader = new BillHeader("测试客户", null, "测试仓库", "刘振峰", null, false, "这是一个单据备注");
-//        BillCell billCellA_BASE = new BillCell("商品档案A", "瓶", "10", 8.80, 88.00, "这是商品A小包单位备注");
-//        BillCell billCellA_PKG = new BillCell("商品档案A", "箱", "10", 88.80, 888.00, "这是商品A大包单位备注");
-//        BillCell billCellB_BASE = new BillCell("商品档案B", "瓶", "10", 9.90, 99.00, "这是商品B小包单位备注");
-//        BillFooter billFooter = new BillFooter(1.10, 2.20, 0.0, 3.30, 0.0, 0.0);
-//
-//        billCellList.add(billCellA_BASE);
-//        billCellList.add(billCellA_PKG);
-//        billCellList.add(billCellB_BASE);
-//
-//        BillWhole billWhole = new BillWhole(billHeader, billCellList, billFooter);
-//        String jsonString = JsonUtils.toJsonStringFromObject(billWhole);
-//        System.out.println(jsonString);
+    /**
+     * 将JSON字符串序列化成JSONObject对象
+     *
+     * @param jsonString JSON字符串
+     * @return JSONObject对象
+     */
+    public static JSONObject toJSONObjectFromJSONString(String jsonString) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = JSON.parseObject(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
 
-        String jsonString = "{\"billCellList\":[{\"currUnitName\":\"瓶\",\"goodsName\":\"商品档案A\",\"quantity\":\"10\"," +
-                "\"realPrice\":8.8,\"remark\":\"这是商品A小包单位备注\",\"subAmount\":88},{\"currUnitName\":\"箱\"," +
-                "\"goodsName\":\"商品档案A\",\"quantity\":\"10\",\"realPrice\":88.8,\"remark\":\"这是商品A大包单位备注\"," +
-                "\"subAmount\":888},{\"currUnitName\":\"瓶\",\"goodsName\":\"商品档案B\",\"quantity\":\"10\"," +
-                "\"realPrice\":9.9,\"remark\":\"这是商品B大包单位备注\",\"subAmount\":99}],\"billFooter\":{\"bank\":0," +
-                "\"cash\":2.2,\"others\":0,\"preCharge\":0,\"prePay\":3.3}," +
-                "\"billHeader\":{\"billRemark\":\"这是一个单据备注\",\"consumerName\":\"测试客户\",\"isBaseUnit\":false," +
-                "\"supplierName\":\"\",\"warehouseName\":\"测试仓库\",\"workOperName\":\"刘振峰\"}}";
-        Object object = JsonUtils.toBeanFromJsonString(jsonString, BillWhole.class);
-        System.out.println(((BillWhole) object).getBillHeader().getWarehouseName());
+    /**
+     * 将JSON字符串序列化成JSONArray对象
+     *
+     * @param jsonString JSON字符串(以"["开头，如"[{"aaa":"bbb"}]")
+     * @return JSONArray对象
+     */
+    public static JSONArray toJSONArrayFromJSONString(String jsonString) {
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = JSON.parseArray(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
+    }
+
+    /**
+     * 将JSON字符串反序列化成JavaBean
+     *
+     * @param jsonString JSON字符串
+     * @param clazz      Java类
+     * @return 对应类JavaBean
+     */
+    public static <T> T toBeanFromJSONString(String jsonString, Class<T> clazz) {
+        T t = null;
+        try {
+            t = JSON.parseObject(jsonString, clazz);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+    /**
+     * 将JSON字符串反序列化成JavaBean集合
+     *
+     * @param jsonString JSON字符串
+     * @param clazz      Java类
+     * @return 对应类JavaBean集合
+     */
+    public static <T> List<T> toListBeanFromJSONString(String jsonString, Class<T> clazz) {
+        List<T> list = new ArrayList<>();
+        try {
+            list = JSON.parseArray(jsonString, clazz);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    /**
+     * 将JSON字符串序列化成MAP集合
+     *
+     * @param jsonString JSON字符串(以"["开头，如"[{"aaa":"bbb"}]")
+     * @return MAP集合
+     */
+    public static List<Map<String, Object>> toListMapFromJSONString(String jsonString) {
+        List<Map<String, Object>> list = new ArrayList<>();
+        try {
+            list = JSON.parseObject(jsonString, new TypeReference<List<Map<String, Object>>>() {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    /**
+     * 从输入流中反序列化成JavaBean
+     *
+     * @param is    输入流
+     * @param clazz Java类
+     * @return 对应类JavaBean
+     */
+    public static <T> T toJavaBeanFromJSONFile(InputStream is, Type clazz) {
+        T t = null;
+        try {
+            t = JSON.parseObject(is, clazz);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
     }
 }
--- src/main/java/com/keyword/automation/customer/JsonKeyword.java
+++ src/main/java/com/keyword/automation/customer/JsonKeyword.java
@@ -1,16 +1,12 @@
 package com.keyword.automation.customer;
 
+import com.alibaba.fastjson.JSONObject;
 import com.keyword.automation.base.common.Constants;
-import com.keyword.automation.base.utils.JsonUtils;
-import com.keyword.automation.bean.BillWhole;
-import com.keyword.automation.database.domain.Goods;
+import com.keyword.automation.base.utils.JSONUtils;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * 自定义Json操作关键字
@@ -21,47 +17,10 @@ public class JsonKeyword {
     private JsonKeyword() {
     }
 
-    /**
-     * 将指定JSON文件转换为map对象
-     *
-     * @param jsonPath 指定JSON文件
-     * @return map对象
-     */
-    public static Map<String, Object> convertToMapFromJsonFile(String jsonPath) {
-        String jsonString = readJsonFile(jsonPath);
-        return JsonUtils.toMapFromJson(jsonString);
-    }
+    public static Object initData(String jsonFilePath, String testCaseSeq) {
+        String jsonString = readJsonFile(jsonFilePath);
+        JSONObject jsonObject = JSONUtils.toJSONObjectFromJSONString(jsonString).getJSONObject(testCaseSeq);
 
-    /**
-     * 从初始化数据中取出对应测试用例场景的数据并转化为对应Java对象
-     *
-     * @param initData     初始化数据
-     * @param testCaseName 测试用例场景key
-     * @return java对象
-     */
-    public static Object convertToJavaBeanObject(Map<String, Object> initData, String testCaseName) {
-        Object obj = initData.get(testCaseName);
-        Set set = ((HashMap) obj).keySet();
-        String classKey = null;
-        String classValue = null;
-        for (Object key : set) {
-            String tempKey = (String) key;
-            if (!tempKey.equalsIgnoreCase("result")) {
-                classKey = tempKey;
-                classValue = JsonUtils.toJsonStringFromObject(((HashMap) obj).get(classKey));
-            }
-        }
-        if (classKey != null) {
-            if (classKey.equalsIgnoreCase("billWhole")) {
-                return JsonUtils.toBeanFromJsonString(classValue, BillWhole.class);
-            } else if (classKey.equalsIgnoreCase("goods")) {
-                return JsonUtils.toBeanFromJsonString(classValue, Goods.class);
-            } else {
-                throw new RuntimeException("没有找到对应的Java Bean.");
-            }
-        } else {
-            throw new RuntimeException("没有找到对应的Java Bean.");
-        }
     }
 
     /**
@@ -98,53 +57,51 @@ public class JsonKeyword {
     }
 
     public static void main(String[] args) {
-        String jsonString = "{\n" +
-                "      \"billHeader\": {\n" +
-                "        \"consumerName\": \"测试客户\",\n" +
-                "        \"supplierName\": null,\n" +
-                "        \"warehouseName\": \"测试仓库\",\n" +
-                "        \"workOperName\": \"刘振峰\",\n" +
-                "        \"workTime\": null,\n" +
-                "        \"isBaseUnit\": false,\n" +
-                "        \"billRemark\": \"这是一个单据备注\"\n" +
-                "      },\n" +
-                "      \"billCellList\": [\n" +
-                "        {\n" +
-                "          \"goodsName\": \"商品档案A\",\n" +
-                "          \"currUnitName\": \"瓶\",\n" +
-                "          \"quantity\": \"10\",\n" +
-                "          \"realPrice\": 8.8,\n" +
-                "          \"subAmount\": 88,\n" +
-                "          \"remark\": \"这是商品A小包单位备注\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"goodsName\": \"商品档案A\",\n" +
-                "          \"currUnitName\": \"箱\",\n" +
-                "          \"quantity\": \"10\",\n" +
-                "          \"realPrice\": 88.8,\n" +
-                "          \"subAmount\": 888,\n" +
-                "          \"remark\": \"这是商品A大包单位备注\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"goodsName\": \"商品档案B\",\n" +
-                "          \"currUnitName\": \"瓶\",\n" +
-                "          \"quantity\": \"10\",\n" +
-                "          \"realPrice\": 9.9,\n" +
-                "          \"subAmount\": 99,\n" +
-                "          \"remark\": \"这是商品B大包单位备注\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"billFooter\": {\n" +
-                "        \"discountAmount\": 1.10,\n" +
-                "        \"cash\": 2.2,\n" +
-                "        \"bank\": 0,\n" +
-                "        \"prePay\": 3.3,\n" +
-                "        \"preCharge\": 0,\n" +
-                "        \"others\": 0\n" +
-                "      }\n" +
-                "    }";
-        Object object = JsonUtils.toBeanFromJsonString(jsonString, BillWhole.class);
-        System.out.println(111);
-        System.out.println(((BillWhole) object).getBillCellList().get(0).getGoodsName());
+//        String jsonString = "{\n" +
+//                "      \"billHeader\": {\n" +
+//                "        \"consumerName\": \"测试客户\",\n" +
+//                "        \"supplierName\": null,\n" +
+//                "        \"warehouseName\": \"测试仓库\",\n" +
+//                "        \"workOperName\": \"刘振峰\",\n" +
+//                "        \"workTime\": null,\n" +
+//                "        \"isBaseUnit\": false,\n" +
+//                "        \"billRemark\": \"这是一个单据备注\"\n" +
+//                "      },\n" +
+//                "      \"billCellList\": [\n" +
+//                "        {\n" +
+//                "          \"goodsName\": \"商品档案A\",\n" +
+//                "          \"currUnitName\": \"瓶\",\n" +
+//                "          \"quantity\": \"10\",\n" +
+//                "          \"realPrice\": 8.8,\n" +
+//                "          \"subAmount\": 88,\n" +
+//                "          \"remark\": \"这是商品A小包单位备注\"\n" +
+//                "        },\n" +
+//                "        {\n" +
+//                "          \"goodsName\": \"商品档案A\",\n" +
+//                "          \"currUnitName\": \"箱\",\n" +
+//                "          \"quantity\": \"10\",\n" +
+//                "          \"realPrice\": 88.8,\n" +
+//                "          \"subAmount\": 888,\n" +
+//                "          \"remark\": \"这是商品A大包单位备注\"\n" +
+//                "        },\n" +
+//                "        {\n" +
+//                "          \"goodsName\": \"商品档案B\",\n" +
+//                "          \"currUnitName\": \"瓶\",\n" +
+//                "          \"quantity\": \"10\",\n" +
+//                "          \"realPrice\": 9.9,\n" +
+//                "          \"subAmount\": 99,\n" +
+//                "          \"remark\": \"这是商品B大包单位备注\"\n" +
+//                "        }\n" +
+//                "      ],\n" +
+//                "      \"billFooter\": {\n" +
+//                "        \"discountAmount\": 1.10,\n" +
+//                "        \"cash\": 2.2,\n" +
+//                "        \"bank\": 0,\n" +
+//                "        \"prePay\": 3.3,\n" +
+//                "        \"preCharge\": 0,\n" +
+//                "        \"others\": 0\n" +
+//                "      }\n" +
+//                "    }";
+
     }
 }
